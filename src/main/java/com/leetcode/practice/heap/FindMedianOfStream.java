9e60efda36dab620b47a05fd3c51736ae1cef6f6
Rudra Narayan Mishra <rnmishra0309@gmail.com>
2024-05-12 13:07:23 +0000

    new problems added

---
+++ src/main/java/com/leetcode/practice/heap/FindMedianOfStream.java
@@ -0,0 +1,26 @@
+package com.leetcode.practice.heap;
+
+import java.util.*;
+
+public class FindMedianOfStream {
+
+    class StreamData {
+
+        public StreamData() {
+
+        }
+
+        public void insertNum() {
+
+        }
+
+        public Double findMedian() {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
